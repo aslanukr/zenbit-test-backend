f9d862764c71b2beeb5588b2e392212b460c4730
Vitaliy Korostylov <vitaliy.koros@gmail.com>
2023-10-20 01:26:21 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
